d2197dbbb55beba5c2ebd63038a83c118de8975b
dte-84 <dte.solutions.llc@gmail.com>
2026-03-13 03:15:53 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-// vite.config.ts
+import { defineConfig } from 'vite' // <--- This line was likely missing!
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/TheClubhouse/',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        // If a library is looking for these, tell Rollup to stay away
-        './cjs/react.production.min.js',
-        './cjs/react-jsx-runtime.production.min.js'
-      ]
-    },
     commonjsOptions: {
-      include: [/node_modules/],
       transformMixedEsModules: true,
     },
   },
