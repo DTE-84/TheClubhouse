7f0bca02c32c4b22050f004752eda02c70550da3
dte-84 <dte.solutions.llc@gmail.com>
2026-03-15 20:22:25 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    base: process.env.GITHUB_ACTIONS === "true" ? "/TheClubhouse/" : "/",
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:3000",
-          changeOrigin: true,
-        },
-      },
-    },
-    resolve: {
-      alias: {
-        "@shared": "/shared",
-      },
-    },
-    build: {
-      commonjsOptions: {
-        transformMixedEsModules: true,
-      },
-    },
-  };
-});
+export default defineConfig({
+  base: '/TheClubhouse/', 
+  plugins: [react()],
+})
